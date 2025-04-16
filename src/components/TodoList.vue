fe7523cd4dbab650ef6acbaa5e1b8648ce6f9efe
Pedro Henrique de Sousa Barbosa <pedro@appservice.com.br>
2025-04-15 21:30:23 -0300

    commit inicial

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>{{ titulo }}</h1>
+    <input type="text" v-model="Novatarefa">
+    <button @click="adicionarTarefa" >Adicionar Tarefa</button>
+    <ul>
+        <li v-for="Novatarefa in listaTarefas" :key="Novatarefa">{{ Novatarefa }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "TodoList",
+    data() {
+        return{
+            Novatarefa: "",
+            listaTarefas: [],
+            titulo:"ToDo with Vue!!"
+        }
+    },
+    methods: {
+        adicionarTarefa() {
+           this.listaTarefas.unshift(this.Novatarefa)
+           this.Novatarefa = "";
+        }
+    },
+}
+
+</script>
